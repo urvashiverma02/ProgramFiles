cd6671d2be1010afd07f829f322773a7348c5161
urvashiverma02 <72400508+urvashiverma02@users.noreply.github.com>
2020-10-11 18:46:14 +0530

    Created Multiply.cpp
    This file prints multiplication of two numbers

---
+++ Multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+inter main() 
+{
+clrscr();
+cout<<"Size Of Char: "<<size of(char) << " byte" << endl;
+cout<<"Size Of Int: "<<size of(int) << " byte" << endl;
+cout<<"Size Of Float: "<<size of(float) << " byte" << endl;
+cout<<"Size Of Double: "<<size of(double) << " byte" << endl;
+getch();
+return 0;
+}
