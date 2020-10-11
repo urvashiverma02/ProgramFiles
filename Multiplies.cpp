d292fee7438d10a2c42b4732fea9a7798a60bc1e
urvashiverma02 <72400508+urvashiverma02@users.noreply.github.com>
2020-10-11 19:30:38 +0530

    Created Multiplies.cpp
    This file prints multiplication of two numbers

---
+++ Multiplies.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main() 
+{
+float numone, numtwo, multiply;
+clrscr();
+cout<<"Enter Any First Number For Multiplication = ";
+cin>>numone;
+cout<<"Enter Any Second Number For Multiplication = ";
+cin>>numtwo;
+multiply = numone * numtwo;
+cout<<"Multiplication Or Product Of Entered Two Numbers (" << numone << " * " << numtwo << ") = " <<multiply;
+getch();
+return 0;
+}
+
